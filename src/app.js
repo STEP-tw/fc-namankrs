c5769f0707d9774fb8ec4f4bf05be01d93e81a37
naman sharma <44020825+namankrs@users.noreply.github.com>
2019-01-16 15:48:44 +0530

    handling invalid request

--- src/app.js
+++ src/app.js
@@ -1,15 +1,36 @@
 const fs = require("fs");
 
+const send = function(res, statusCode, data = "") {
+  res.statusCode = statusCode;
+  res.write(data);
+  res.end();
+  return;
+};
+
+const handler = function(url, res, statusCode = 200) {
+  fs.readFile(url, (err, data) => {
+    if (err) {
+      send(res, 404, "file not found");
+      return;
+    }
+    send(res, statusCode, data);
+  });
+};
+
 const app = (req, res) => {
+  let currUrl = "";
   if (req.url == "/favicon.ico") {
     res.end();
     return;
   }
-  fs.readFile("." + req.url, (err, data) => {
-    res.statusCode = 200;
-    res.write(data);
-    res.end();
-  });
+  if (req.url == "/") {
+    currUrl = "./src/index.html";
+    handler(currUrl, res);
+    return;
+  }
+  currUrl = "." + req.url;
+  handler(currUrl, res);
+  return;
 };
 
 // Export a function that can act as a handler
