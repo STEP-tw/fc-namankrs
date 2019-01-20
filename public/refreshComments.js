3564701f695244370ce73a14ca99735d5cdc2bfc
naman sharma <44020825+namankrs@users.noreply.github.com>
2019-01-20 15:21:36 +0530

    inroduced refresh button

--- public/refreshComments.js
+++ public/refreshComments.js
@@ -1,9 +1,20 @@
+const NEWLINE = "</br>";
+
+const reverse = data =>
+  data
+    .toString()
+    .split(NEWLINE)
+    .reverse()
+    .join(NEWLINE);
+
 const refreshComments = function() {
-  fetch("/comments.txt").then(function(response) {
-    // if (!response.ok) {
-    //   console.log("not ok");
-    //   return;
-    // }
-    console.log(response.text());
-  });
+  fetch("/comments.txt")
+    .then(function(response) {
+      return response.text();
+    })
+    .then(function(data) {
+      let reversedData = reverse(data);
+      let previousComments = document.getElementById("comments");
+      previousComments.innerHTML = reversedData;
+    });
 };
